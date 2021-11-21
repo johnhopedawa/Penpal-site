5cbcdce7a7009c39a70cac70942d38967fdfc436
unknown <johnhope.dawa@yahoo.com>
2021-11-21 14:12:11 -0800

    click logo to scroll up

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+function clearAll() {
+
+    setTimeout(function(){
+        document.getElementById("first").style.cssText = 'display: none';
+        document.getElementById("second").style.cssText = 'display: none';
+    },500);
+    
+}
 function clickFunction() {
     document.getElementById("first").style.cssText = 'display: flex';
     document.getElementById("second").style.cssText = 'display: none';
